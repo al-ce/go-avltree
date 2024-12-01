3d6e0d7212a690a280a1fcf685017a70b1d38469
al-ce <aceserani@gmail.com>
2024-12-01 16:02:41 -0600

    feat(tree): add Contains/Remove method
    fix(remove): check for empty tree before trying to set parent of root to nil
    
    Add `replaceRoot` helper function
    
    feat(tree): add Contains/Remove method

--- avl.go
+++ avl.go
@@ -32,6 +32,14 @@ func (node *Node[T]) balanceFactor() int {
 
 // %% Public methods %%
 
+// %%% Node public methods %%%
+
+// %%% Tree public methods %%%
+
+func NewAvlTree[T constraints.Ordered]() *AvlTree[T] {
+	return &AvlTree[T]{root: nil}
+}
+
 func (tree *AvlTree[T]) PrintTree(node *Node[T]) {
 	if node == nil {
 		return
@@ -41,8 +49,9 @@ func (tree *AvlTree[T]) PrintTree(node *Node[T]) {
 	tree.PrintTree(node.right)
 }
 
-func NewAvlTree[T constraints.Ordered]() *AvlTree[T] {
-	return &AvlTree[T]{root: nil}
+// Returns a bool indicating whether the value exists in the tree
+func (tree *AvlTree[T]) Contains(value T) bool {
+	return tree.getNodeByValue(value) != nil
 }
 
 func (tree *AvlTree[T]) Add(value T) {
@@ -76,7 +85,63 @@ func (tree *AvlTree[T]) Add(value T) {
 	}
 }
 
+// Remove a node from the tree by value lookup.
+// Returns true on successful removal, false if value was not found.
+func (tree *AvlTree[T]) Remove(value T) bool {
+	node := tree.getNodeByValue(value)
+	if node == nil { // value was not found in the tree
+		return false
+	}
+
+	parent := node.parent
+	var replacement *Node[T]
+
+	// Action node is the node where the rebalancing starts
+	actionNode := parent
+
+	if node.left == nil || node.right == nil { // Case 1: no children or one child
+		if node.right == nil {
+			replacement = node.left
+		} else {
+			replacement = node.right
+		}
+	} else { // Case 2: two children
+		// Find in-order successor (move right once then left all the way down)
+		successor := node.right
+		for successor.left != nil {
+			successor = successor.left
+		}
+
+		// Reassign the links between nodes
+		successor.left = node.left
+		if successor != node.right {
+			successor.parent.left = successor.right
+			if successor.right != nil {
+				successor.right.parent = successor.parent
+			}
+			successor.right = node.right
+		}
+		node.left.parent = successor
+		node.right.parent = successor
+		replacement = successor
+		actionNode = replacement.parent
+	}
+	tree.replaceChild(parent, node, replacement)
+	if replacement != nil {
+		replacement.parent = parent
+	}
+
+	for actionNode != nil {
+		tree.rebalance(actionNode)
+		actionNode = actionNode.parent
+	}
+
+	return true
+}
+
 // %% Private methods %%
+
+// %%% Node private methods %%%
 
 func (node *Node[T]) rotateLeft() *Node[T] {
 	child := node.right
@@ -116,6 +181,27 @@ func (node *Node[T]) updateHeight() {
 		rightHeight = node.right.height
 	}
 	node.height = int(math.Max(float64(leftHeight), float64(rightHeight))) + 1
+}
+
+// %%% Tree private methods %%%
+
+func (tree *AvlTree[T]) getNodeByValue(value T) *Node[T] {
+	if tree.root == nil {
+		return nil
+	}
+
+	node := tree.root
+	for node != nil {
+		if node.value == value {
+			return node
+		}
+		if value < node.value {
+			node = node.left
+		} else {
+			node = node.right
+		}
+	}
+	return nil
 }
 
 func (tree *AvlTree[T]) inorderTraverse(node *Node[T], queue *[]T) {
@@ -162,10 +248,21 @@ func (tree *AvlTree[T]) rebalance(node *Node[T]) {
 	tree.replaceChild(nodeParent, node, newSubtreeRoot)
 }
 
+func (tree *AvlTree[T]) replaceRoot(newRoot *Node[T]) {
+	tree.root = newRoot
+	if newRoot != nil {
+		newRoot.parent = nil
+	}
+}
+
 func (tree *AvlTree[T]) replaceChild(parent *Node[T], child *Node[T], replacement *Node[T]) {
+	// If we are replacing the root node
 	if parent == nil {
-		tree.root = replacement
-	} else if parent.left == child {
+		tree.replaceRoot(replacement)
+		return
+	}
+
+	if parent.left == child {
 		parent.left = replacement
 	} else {
 		parent.right = replacement
--- avl_test.go
+++ avl_test.go
@@ -5,28 +5,59 @@ import (
 	"testing"
 )
 
+func rangeWithSteps(start, end, step int) []int {
+	var result []int
+	for i := start; i <= end; i += step {
+		result = append(result, i)
+	}
+	return result
+}
+
+var cases = [][]int{
+	{1, 2, 3},             // Right-Right case
+	{1, 3, 2},             // Right-Left case
+	{2, 1, 3},             // Left-Left case
+	{2, 3, 1},             // Left-Right case
+	{3, 1, 2},             // Left-Right case
+	{3, 2, 1},             // Left-Left case
+	{10, 20, 30, 40, 50},  // Multiple Right-Right rotations
+	{10, 20, 30, 50, 40},  // Right-Right followed by Right-Left
+	{30, 20, 10, 5, 1},    // Multiple Left-Left rotations
+	{30, 20, 10, 1, 5},    // Left-Left followed by Left-Right
+	{5, 4, 6, 3, 7, 2, 8}, // Mixed rotations
+	{-1, -2, -3},          // Negative values
+	{-5, -3, 1, 3, 5},     // Negative and positive values
+	{50, 40, 60, 30, 70, 20, 80, 45},
+	{50, 40, 60, 30, 70, 20, 80, 15},
+	{50, 40, 60, 30, 70, 20, 80, 35},
+	{50, 40, 60, 30, 70, 20, 80, 25},
+	rangeWithSteps(-9, 16, 2),
+	rangeWithSteps(0, 34, 3),
+}
+
+func deepCopyTestCases(original [][]int) [][]int {
+	sliceCopy := make([][]int, len(original))
+	for i, slice := range original {
+		sliceCopy[i] = make([]int, len(slice))
+		copy(sliceCopy[i], slice)
+	}
+	return sliceCopy
+}
+
+func populateTree(values []int) *AvlTree[int] {
+	tree := NewAvlTree[int]()
+	for _, value := range values {
+		tree.Add(value)
+	}
+	return tree
+}
+
 // Tests the AVL tree with integer values, covering all basic rotation cases
 func TestIntegerTree(t *testing.T) {
-	cases := [][]int{
-		{1, 2, 3},             // Right-Right case
-		{1, 3, 2},             // Right-Left case
-		{2, 1, 3},             // Left-Left case
-		{2, 3, 1},             // Left-Right case
-		{3, 1, 2},             // Left-Right case
-		{3, 2, 1},             // Left-Left case
-		{10, 20, 30, 40, 50},  // Multiple Right-Right rotations
-		{10, 20, 30, 50, 40},  // Right-Right followed by Right-Left
-		{30, 20, 10, 5, 1},    // Multiple Left-Left rotations
-		{30, 20, 10, 1, 5},    // Left-Left followed by Left-Right
-		{5, 4, 6, 3, 7, 2, 8}, // Mixed rotations
-	}
+	cases = deepCopyTestCases(cases)
 
 	for i, testCase := range cases {
-		tree := NewAvlTree[int]()
-
-		for _, value := range testCase {
-			tree.Add(value)
-		}
+		tree := populateTree(testCase)
 		actual := tree.getTreeValues(tree.root)
 		expected := slices.Clone(testCase)
 		slices.Sort(expected)
@@ -66,9 +97,9 @@ func TestStringTree(t *testing.T) {
 // Tests the AVL tree with floating-point values
 func TestFloatTree(t *testing.T) {
 	cases := [][]float64{
-		{1.1, 2.2, 3.3},             // Ordered
-		{3.3, 2.2, 1.1},             // Reversed
-		{2.2, 1.1, 3.3},             // Mixed
+		{1.1, 2.2, 3.3}, // Ordered
+		{3.3, 2.2, 1.1}, // Reversed
+		{2.2, 1.1, 3.3}, // Mixed
 	}
 
 	for i, testCase := range cases {
@@ -84,6 +115,88 @@ func TestFloatTree(t *testing.T) {
 
 		if !slices.Equal(actual, expected) {
 			t.Errorf("Test case %d: tree.Add(...) = %v; want %v", i, actual, expected)
+		}
+	}
+}
+
+// Test Contains method, indicating whether a value exists in the AVL tree
+func TestContains(t *testing.T) {
+	cases = deepCopyTestCases(cases)
+	for _, testCase := range cases {
+		values := testCase
+		tree := populateTree(values)
+		for _, v := range values {
+			actual := tree.Contains(v)
+			expected := true
+			if actual != expected {
+				t.Errorf("Test contains value: tree.Contains(%v) = %v; want %v", v, actual, expected)
+			}
+		}
+	}
+}
+
+func TestDoesNotContain(t *testing.T) {
+	tree := populateTree([]int{1, 2, 3})
+	actual := tree.Contains(4)
+	expected := false
+	if actual != expected {
+		t.Errorf("Test contains value: tree.Contains(4) = %v; want %v", actual, expected)
+	}
+}
+
+// Test removing a value that does not exist in the AVL tree
+func TestRemoveNonexistingValue(t *testing.T) {
+	values := []int{1, 2, 3}
+	tree := populateTree(values)
+	actual := tree.Remove(0)
+	expected := tree.Contains(0)
+	if actual != expected {
+		t.Errorf("Test remove non-existing value: tree.Remove(0) = %v; want %v", actual, expected)
+	}
+}
+
+// Test removing a value that does not exist in the AVL tree
+func TestRemoveValues(t *testing.T) {
+	for i, testCase := range cases {
+		for j, v := range testCase {
+			tree := populateTree(testCase)
+
+			// Successful remove returns true, so negate this to check against
+			// `Contains(value) == false`
+
+			actual := !tree.Remove(v)
+			expected := tree.Contains(v)
+
+			if actual != expected {
+				t.Errorf("Test case %d.%d: tree.Remove(%v) = %v; want %v", i+1, j, v, actual, expected)
+			}
+
+			// Ensure order was maintained during removal
+			actualValues := tree.getTreeValues(tree.root)
+			expectedValues := slices.Clone(actualValues)
+			slices.Sort(expectedValues)
+			if !slices.Equal(actualValues, expectedValues) {
+				t.Errorf("Test case %d.%d: tree.Remove(%v) = %v; want %v", i+1, j, v, actual, expected)
+			}
+
+		}
+	}
+}
+
+// Test removing multiple values until the tree is empty
+func TestRemoveMultipleValues(t *testing.T) {
+	for i, testCase := range cases {
+		tree := populateTree(testCase)
+		for j, v := range testCase {
+
+			// Successful remove returns true, so negate this to check against
+			// `Contains(value) == false`
+			actual := !tree.Remove(v)
+			expected := tree.Contains(v)
+
+			if actual != expected {
+				t.Errorf("Test case %d.%d: tree.Remove(%v) = %v; want %v", i+1, j, v, actual, expected)
+			}
 		}
 	}
 }
